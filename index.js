6c58ad6eb33f002a30df8d37aadea28920089d59
Kapoors-Hub <pranjil@mousencheese.design>
2024-09-26 19:19:54 +0530

    last

--- index.js
+++ index.js
@@ -7,20 +7,21 @@ import cors from "cors"
 dotenv.config()
 
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
-    console.log("Connected to mongoDB")
+    console.log("Connected to MongoDB Atlas");
   })
   .catch((err) => {
-    console.log(err)
-  })
+    console.log(err);
+  });
 
 const app = express()
 
 // to make input as json
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
+// app.use(cors({ origin: ["http://localhost:5173"], credentials: true }))
+app.use(cors({ credentials: true }));
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000")
