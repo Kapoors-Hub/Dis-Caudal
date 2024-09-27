1fbbd93edb035636d8f5d490a35bd7f4bdc7af6b
Kapoors-Hub <pranjil@mousencheese.design>
2024-09-27 13:56:36 +0530

    s

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -55,6 +55,7 @@ export const signin = async (req, res, next) => {
 
     res.cookie("access_token", token, { httpOnly: true }).status(200).json({
       success: true,
+      httpOnly: true,
       secure: process.env.NODE_ENV === "production", // Use secure cookies in production
       sameSite: "None", // Allows cross-site requests
       message: "Login Successful!",
